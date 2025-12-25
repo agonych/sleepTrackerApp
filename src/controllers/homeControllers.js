f4a3d7b5259b8901fad58b8a36d41e5b4ccfeb63
Andrej Kudriavcev <andrej@kudriavcev.info>
2025-12-25 23:45:58 +1100

    Closes #US0-1-10: Configure base EJS structure for view and components

--- src/controllers/homeControllers.js
+++ src/controllers/homeControllers.js
@@ -2,7 +2,7 @@
  * Home page controller handlers.
  */
 function renderHome(req, res) {
-  res.render('index', { appName: 'Alive Sleep Tracker App' });
+  res.render('pages/home', { appName: 'Alive Sleep Tracker App' });
 }
 
 module.exports = {
